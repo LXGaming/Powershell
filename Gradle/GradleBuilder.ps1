b484fe98587b43d7379e31a01686e2a76d43462c
Alex Thomson <alexrobthomson@outlook.com>
2017-02-21 15:38:59 +1300

    Scripts used for updating Gradle projects.

---
+++ Gradle/GradleBuilder.ps1
@@ -0,0 +1,26 @@
+﻿Clear-Host
+$Base = Get-ChildItem -Directory "" -Depth 1
+
+ForEach ($Folder in $Base) {
+  if ((Test-Path ($Folder.FullName + "\.gradle")) -And (Test-Path ($Folder.FullName + "\build.gradle")) -And (Test-Path ($Folder.FullName + "\gradlew"))) {
+    Write-Host ("Processing: " + $Folder.Name)
+    Remove-Item ($Folder.FullName + "\.gradle") -Recurse
+    Write-Host ("Removed: " + $Folder.FullName + "\.gradle");
+
+    $ProcessStartInfo = New-Object System.Diagnostics.ProcessStartInfo;
+    $ProcessStartInfo.FileName = ($Folder.FullName + "\gradlew.bat");
+    $ProcessStartInfo.RedirectStandardError = $True
+    $ProcessStartInfo.RedirectStandardOutput = $True
+    $ProcessStartInfo.UseShellExecute = $False
+    $ProcessStartInfo.CreateNoWindow = $True
+    $ProcessStartInfo.Arguments = ("-p "  + $Folder.FullName + " clean build");
+    Write-Host ("Process Arguments: " + $ProcessStartInfo.Arguments);
+    $Process = New-Object System.Diagnostics.Process;
+    $Process.StartInfo = $ProcessStartInfo;
+    $Process.Start() | Out-Null;
+    $Process.WaitForExit();
+    $Output = $Process.StandardOutput.ReadToEnd();
+    $Output += $Process.StandardError.ReadToEnd();
+    Write-Host $Output;
+  }
+}
---
+++ Gradle/GradleUpdater.ps1
@@ -0,0 +1,27 @@
+﻿Clear-Host
+$GradleVersion = "3.4"
+$Base = Get-ChildItem -Directory "" -Depth 1
+
+ForEach ($Folder in $Base) {
+  if ((Test-Path ($Folder.FullName + "\.gradle")) -And (Test-Path ($Folder.FullName + "\build.gradle")) -And (Test-Path ($Folder.FullName + "\gradlew"))) {
+    Write-Host ("Processing: " + $Folder.Name)
+    Remove-Item ($Folder.FullName + "\.gradle") -Recurse
+    Write-Host ("Removed: " + $Folder.FullName + "\.gradle");
+
+    $ProcessStartInfo = New-Object System.Diagnostics.ProcessStartInfo;
+    $ProcessStartInfo.FileName = ($Folder.FullName + "\gradlew.bat");
+    $ProcessStartInfo.RedirectStandardError = $True
+    $ProcessStartInfo.RedirectStandardOutput = $True
+    $ProcessStartInfo.UseShellExecute = $False
+    $ProcessStartInfo.CreateNoWindow = $True
+    $ProcessStartInfo.Arguments = ("-p "  + $Folder.FullName + " wrapper --gradle-version " + $GradleVersion);
+    Write-Host ("Process Arguments: " + $ProcessStartInfo.Arguments);
+    $Process = New-Object System.Diagnostics.Process;
+    $Process.StartInfo = $ProcessStartInfo;
+    $Process.Start() | Out-Null;
+    $Process.WaitForExit();
+    $Output = $Process.StandardOutput.ReadToEnd();
+    $Output += $Process.StandardError.ReadToEnd();
+    Write-Host $Output;
+  }
+}
